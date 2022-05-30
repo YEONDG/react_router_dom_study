8f748dc4fd1710386a996c16d064683a0404bef2
YDG <zzmn1234@naver.com>
2022-05-30 14:13:41 +0900

    add comments

--- src/components/quotes/NoQuotesFound.js
+++ src/components/quotes/NoQuotesFound.js
@@ -1,12 +1,13 @@
-import classes from './NoQuotesFound.module.css';
+import { Link } from "react-router-dom";
+import classes from "./NoQuotesFound.module.css";
 
 const NoQuotesFound = () => {
   return (
-    <div className={classes.noquotes}>
+    <div className={classes.nquotes}>
       <p>No quotes found!</p>
-      <a className='btn'>
+      <Link className="btn" to="/new-quote">
         Add a Quote
-      </a>
+      </Link>
     </div>
   );
 };
